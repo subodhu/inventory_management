11d676976dec8c8ac5860aba0e5e1581f1085504
Subodh Upreti <subodh.ur@gmail.com>
2022-05-07 15:36:44 +0545

    homepage redirected to inventory list

--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -20,7 +20,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
+    path("", RedirectView.as_view(url='/inventory/'), name="home"),
     path('admin/', admin.site.urls),
     path("", include("inventory.urls", namespace='inventory')),
     path("api/", include("inventory.api.urls", namespace='inventory_api')),
