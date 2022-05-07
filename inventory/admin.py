c02cec5edff11e99493ac11ff466215ec9c19b65
Subodh Upreti <subodh.ur@gmail.com>
2022-05-07 13:32:43 +0545

    Inventory and Suppliers model added in django admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Inventory, Supplier
+
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stock', 'availability', 'supplier')
+    list_filter = ('name',)
+    ordering = ['-created_at']
+    select_related_fields = ('supplier', )
+
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    ordering = ['-created_at']
